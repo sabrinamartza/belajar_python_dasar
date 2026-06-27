0ac9c5224d5b0b97e0d197bf53d4ceb6a58e1075
pino <sabrinamartza07@gmail.com>
2026-06-27 23:15:18 +0700

    menambahkan file Latihan.py

---
+++ Latihan.py
@@ -0,0 +1,35 @@
+#ini adalah program pertama aku :)
+Nama = "Pyno" #karena dia adalah teks maka pake tanda petik ya 
+Semangat = False #bisa ubah ubah true dan false
+
+print("===Program Semangat===")
+print("Halo nama saya "+Nama)
+
+if Semangat == True:
+    print("saya pasti bisa ngoding") #ini yang bakal muncul kalau kamu tulis true di baris ke 3
+else:
+    print("tetep semangat ya belajarnya!") #ini kalau kamu tulis false bakal keluar tulisan ini
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
