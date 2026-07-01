91754ae88d58ebc4bc5422d0ccdc7f21446b061f
pino <sabrinamartza07@gmail.com>
2026-07-01 15:29:41 +0700

    belajar perulangan

---
+++ latihan_loop.py
@@ -0,0 +1,21 @@
+total = 5
+for i in range(3):
+    total -= 1
+#ini baca nya total = 5 di kurangi 3x di nah pengurangan nya itu -1
+print(total)
+
+
+total = 50
+for i in range (2):
+    total += 5 
+print(total)
+
+number = 40
+for i in range (1): #berapa banyak ini berulang kalau ku tulis satu ya di bagi 1x kalau 2 ya 2x 
+    number /= 4 # di bagi 4 atau angka berapapun yang kamu masukin
+print(number)
+
+number = 5
+for i in range (2):
+    number *=2
+    print(number)
